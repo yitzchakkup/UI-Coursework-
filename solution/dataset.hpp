93f5926d520812dce6180261a5588ba5c48358c8
benharrisss <146212225+benharrisss@users.noreply.github.com>
2024-12-08 14:32:06 +0000

    Update dataset.hpp

--- solution/dataset.hpp
+++ solution/dataset.hpp
@@ -13,6 +13,14 @@ public:
   void loadData(const std::string &);
   int size() const { return data.size(); }
   Water operator[](int index) const { return data.at(index); }
+
+  std::vector<std::pair<std::string, double>> getPollutants(const std::string& pollutantName, const std::string& location);
+  std::vector<std::string> getLocations(const std::string& pollutantName);
+
+  std::vector<std::string> getLabels();
+
+  std::vector<Water> getData();
+  std::vector<Water> getPOPs();
   /*
   Quake strongest() const;
   Quake shallowest() const;
@@ -23,5 +31,12 @@ public:
 private:
   std::vector<Water> data;
   std::vector<std::string> pollutants;
+  //std::vector<std::PollutantResults> popResults;
+  std::vector<Water> pops;
+
+  std::vector<std::string> labels;
+
+  //Water record;
+
   void checkDataExists() const;
 };
