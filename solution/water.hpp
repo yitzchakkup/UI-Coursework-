63177de3243e72152c28fba912c9198bf8ca20db
benharrisss <146212225+benharrisss@users.noreply.github.com>
2024-12-08 14:37:03 +0000

    Update water.hpp

--- solution/water.hpp
+++ solution/water.hpp
@@ -14,10 +14,10 @@ public:
   SamplingPoint(std::string notation, float easting, float northing, std::string label)
       : notation(notation), easting(easting), northing(northing), label(label) {}
 
-  std::string getNotation() { return notation; }
-  std::string getLabel() { return label; }
-  float getEasting() { return easting; }
-  float getNorthing() { return northing; }
+  std::string getNotation() const { return notation; }
+  std::string getLabel() const { return label; }
+  float getEasting() const { return easting; }
+  float getNorthing() const { return northing; }
 
 private:
   std::string notation;
@@ -31,10 +31,10 @@ class Determinand
 public:
   Determinand(std::string label, std::string definition, std::string notation, std::string unit)
       : label(label), definition(definition), notation(notation), unit(unit) {}
-  std::string getLabel() { return label; }
-  std::string getDefinition() { return definition; }
-  std::string getNotation() { return notation; }
-  std::string getUnit() { return unit; }
+  std::string getLabel() const { return label; }
+  std::string getDefinition() const { return definition; }
+  std::string getNotation() const { return notation; }
+  std::string getUnit() const { return unit; }
   friend class Water;
 
 private:
@@ -59,11 +59,11 @@ public:
          std::string dateTime, bool isCompliance)
       : samplingPoint(samplingPoint), purpose(purpose), materialType(materialType),
         dateTime(dateTime), isCompliance(isCompliance) {}
-  std::string getPurpose() { return purpose; }
-  std::string getMaterialType() { return materialType; }
-  std::string getDateTime() { return dateTime; }
-  bool getIsCompliance() { return isCompliance; }
-  SamplingPoint getSamplingPoint() { return samplingPoint; }
+  std::string getPurpose() const { return purpose; }
+  std::string getMaterialType() const { return materialType; }
+  std::string getDateTime() const { return dateTime; }
+  bool getIsCompliance() const { return isCompliance; }
+  SamplingPoint getSamplingPoint() const { return samplingPoint; }
 };
 
 class Water
@@ -79,12 +79,12 @@ private:
 public:
   Water(const std::string &id, float result, std::string resultQualifier, std::string resultInterpretation,
         Sample sample, Determinand determinand);
-  std::string getId() { return id; }
-  float getResult() { return result; }
-  std::string getResultQualifier() { return resultQualifier; }
-  std::string getResultInterpretation() { return resultInterpretation; }
-  Determinand getDeterminand() { return determinand; }
-  Sample getSample() { return sample; }
+  std::string getId() const { return id; }
+  float getResult() const { return result; }
+  std::string getResultQualifier() const { return resultQualifier; }
+  std::string getResultInterpretation() const { return resultInterpretation; }
+  Determinand getDeterminand() const { return determinand; }
+  Sample getSample() const { return sample; }
 };
 
 std::ostream &operator<<(std::ostream &, const Water &);
